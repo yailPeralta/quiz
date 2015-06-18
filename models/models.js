5e97d6a281e39a06dbf817a194937e03cf87984b
Yail Peralta <yailpz@hotmail.com>
2015-06-17 23:07:11 -0300

    Depliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -4,27 +4,30 @@
 var path = require('path');
 var Sequelize = require('sequelize');
 
-var database_url = process.env.DATABASE_URL || 'sqlite://:@:/';
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 
-var url = database_url.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var dbName  = (url[6] || null);
-var user    = (url[2] || null);
-var pwd     = (url[3] || null);
-var dialect = (url[1] || null);
-var port    = (url[5] || null);
-var host    = (url[4] || null);
-var storage = process.env.DATABASE_STORAGE || 'quiz.sqlite';
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd,
+    { dialect:  protocol,
+        protocol: protocol,
+        port:     port,
+        host:     host,
+        storage:  storage,  // solo SQLite (.env)
+        omitNull: true      // solo Postgres
+    }
+);
 
-var sequelize =  new Sequelize(dbName, user, pwd, {
-    dialect: dialect,
-    storage: storage,
-    protocol: dialect,
-    port: port,
-    host: host,
-    omitNull: true
-});
-
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+// Importar definicion de la tabla Quiz
+var quiz_path = path.join(__dirname,'quiz');
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz;
 
@@ -34,7 +37,8 @@ sequelize.sync().then(function () {
             Quiz.create({
                 pregunta: "Capital de Italia",
                 respuesta: "Roma"
-            }).then(function () {
+            })
+            .then(function () {
                 console.log('Base de datos inicializada!');
             })
         }
